f6ba5fe7c413bd80df2978e1c6b4a5e33573a962
LaloCode <lalo_pichu22@hotmail.com>
2022-11-09 21:44:50 -0600

    Countries exercise start

---
+++ countries/src/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const App = ({ countries }) => {
+  const [filter, setFilter] = useState('')
+  const [showCountries, setCountries] = useState([])
+
+  return (
+    <div>Hi</div>
+  );
+}
+
+export default App;
