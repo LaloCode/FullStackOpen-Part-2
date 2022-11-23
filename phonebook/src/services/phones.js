4ea7463c0620701733911e1181cd92db8fb49cf1
LaloCode <lalo_pichu22@hotmail.com>
2022-11-22 18:10:40 -0600

    get rid of lint warning

--- phonebook/src/services/phones.js
+++ phonebook/src/services/phones.js
@@ -13,4 +13,9 @@ const create = (newObject) => {
   return request.then((response) => response.data);
 };
 
-export default { getAll, create };
+const phoneService = {
+  getAll,
+  create
+}
+
+export default phoneService;
