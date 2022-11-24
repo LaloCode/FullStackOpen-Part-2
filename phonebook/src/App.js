5d03b143d4977f680d1a8c2d1634ea6cbfc8692a
LaloCode <lalo_pichu22@hotmail.com>
2022-11-23 18:13:59 -0600

    Add notification message on addition or edit

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -64,11 +64,20 @@ const Person = ({ person, persons, setPersons }) => {
   );
 };
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return <div className="notification"> {message} </div>;
+};
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
   const [filter, setNewFilter] = useState('');
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     phoneService.getAll().then((persons) => {
@@ -106,6 +115,10 @@ const App = () => {
             )
           );
         });
+        setMessage(`${person.name} number was changed.`);
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
       }
       return;
     }
@@ -114,12 +127,17 @@ const App = () => {
       setPersons(persons.concat(returnedPerson));
       setNewName('');
       setNewPhone('');
+      setMessage(`${person.name} was added.`);
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
     });
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h3>Add a new</h3>
       <PersonForm
